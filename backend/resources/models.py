10386e0accd6fbeee0862461daa88b12fe1c8fe0
luanromerolcc <lrlcc@cin.ufpe.br>
2026-02-28 15:41:50 -0300

    models functional

---
+++ backend/resources/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+class Resource(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    TypeChoices = [ ('pdf', 'PDF'), ('video', 'Vídeo'), ('link','Link') ]
+    type = models.CharField(max_length=10, choices=TypeChoices)
+    description = models.TextField() #obligatory field for now, may change later
+    tags = models.JSONField(default=list)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta: 
+        ordering = ['-creation_date']    #order by date from most to least recent resource
