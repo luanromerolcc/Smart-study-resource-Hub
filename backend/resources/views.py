e9bd5abaf9f1a6e83b28e1dbb5a809ad0286c3d9
luanromerolcc <lrlcc@cin.ufpe.br>
2026-02-28 21:34:07 -0300

    views halfway done

---
+++ backend/resources/views.py
@@ -0,0 +1,6 @@
+from .models import Resource
+from .serializer import ResourceSerializer
+from rest_framework import viewsets, status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
