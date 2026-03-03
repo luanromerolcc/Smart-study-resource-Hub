64776d7dcfc931f891d7763ac6cd84ad3a198083
luanromerolcc <lrlcc@cin.ufpe.br>
2026-03-03 15:51:17 -0300

    comments and front end api connection

---
+++ frontend/src/services/api.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+
+const api = axios.create({
+    baseURL: 'http://localhost:8000/api',
+})
+
+//get all resources
+export const getResources = (page = 1) =>
+    api.get(`/resources/${id}/`)
+
+//get single resource
+export const getResource = (id) =>
+    api.get(`/resources/${id}/`)
+
+//create new resource
+export const createResource = (data) =>
+    api.post('/resources/', data)
+
+//update resource (using id)
+export const updateResource = (id, data) =>
+    api.put(`/resources/${id}/`, data)
+
+//delete resource (using id)
+export const deleteResource = (id) =>
+    api.delete(`/resources/${id}/`)
+
+//call smart assist
+export const smartAssist = (data) =>
+    api.post('/smart-assist/', data)
